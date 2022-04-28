d88c157dd94a08d76ca03fb48c12e363cfcb8b52
VuHuong511 <84625353+VuHuong511@users.noreply.github.com>
2022-04-28 21:13:21 +0700

    identity, sendmail, stores, authorization

---
+++ CodeWEB/CodeWEB/Migrations/20220428134549_AddRole.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeWEB.Migrations
+{
+    public partial class AddRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "AspNetUsers");
+        }
+    }
+}
